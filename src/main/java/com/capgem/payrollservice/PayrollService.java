14226ec64bb679725c4892d5e399869d1078042a
Zeeshan91490 <www.zeeshankhan91490@gmail.com>
2020-11-06 22:59:19 +0530

    [zeeshankhan]ability to add employee to multiple tables

---
+++ src/main/java/com/capgem/payrollservice/PayrollService.java
@@ -0,0 +1,41 @@
+package com.capg.payrollservice;
+
+import java.sql.*;
+import java.util.Enumeration;
+
+public class PayrollService {
+
+	public static final String URL = "jdbc:mysql://localhost:3306/payroll_service";
+	public static final String USER = "root";
+	public static final String PASSWORD = "Star@Sun98";
+	public static Connection connection = null;
+	private static int connectionCounter = 0;
+
+	public static Connection getConnection() {
+		connectionCounter++;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			System.out.println("Driver successfully loaded");
+		
+			System.out.println("Processing thread: "+Thread.currentThread().getName()+" Connecting to database with Id: "+connectionCounter);
+			connection = DriverManager.getConnection(URL, USER, PASSWORD);
+			System.out.println("Processing thread: "+Thread.currentThread().getName()+" Id: "+connectionCounter+ " Connection is successfull!" + connection);
+		} catch (ClassNotFoundException e) {
+			throw new IllegalStateException("Cannot find driver", e);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		listDrivers();
+		return connection;
+	}
+
+	public static void listDrivers() {
+		Enumeration<Driver> driverList = DriverManager.getDrivers();
+		while (driverList.hasMoreElements()) {
+			Driver driverClass = (Driver) driverList.nextElement();
+			System.out.println(" " + driverClass.getClass());
+		}
+	}
+}
+
+
