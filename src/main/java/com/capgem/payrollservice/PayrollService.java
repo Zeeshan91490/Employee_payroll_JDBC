fb61cbbfd3322bbb5dec3f506b777aab3d73fcb2
Zeeshan91490 <www.zeeshankhan91490@gmail.com>
2020-11-06 23:10:48 +0530

    [zeeshankhan]add:ability to register driver and establish connection

--- src/main/java/com/capgem/payrollservice/PayrollService.java
+++ src/main/java/com/capgem/payrollservice/PayrollService.java
@@ -8,25 +8,20 @@ public class PayrollService {
 	public static final String URL = "jdbc:mysql://localhost:3306/payroll_service";
 	public static final String USER = "root";
 	public static final String PASSWORD = "Star@Sun98";
-	public static Connection connection = null;
-	private static int connectionCounter = 0;
 
-	public static Connection getConnection() {
-		connectionCounter++;
+	public static void main(String[] args) {
+		Connection connection;
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			System.out.println("Driver successfully loaded");
-		
-			System.out.println("Processing thread: "+Thread.currentThread().getName()+" Connecting to database with Id: "+connectionCounter);
+			System.out.println("Driver loaded!");
+			listDrivers();
 			connection = DriverManager.getConnection(URL, USER, PASSWORD);
-			System.out.println("Processing thread: "+Thread.currentThread().getName()+" Id: "+connectionCounter+ " Connection is successfull!" + connection);
+			System.out.println(connection + " Connection established!");
 		} catch (ClassNotFoundException e) {
-			throw new IllegalStateException("Cannot find driver", e);
+			throw new IllegalStateException("Cannot find the driver in the classpath", e);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		listDrivers();
-		return connection;
 	}
 
 	public static void listDrivers() {
@@ -37,5 +32,3 @@ public class PayrollService {
 		}
 	}
 }
-
-
